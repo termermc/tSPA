5f7160a9fe519007f7d3299ad28a7c95ddb66fab
Michael Termer <termer@protonmail.com>
2018-11-07 01:13:24 -0500

    v1.2

--- tSPA.js
+++ tSPA.js
@@ -1,147 +1,189 @@
 function getDomain(url) {
-    return url.replace("http://", "").replace("https://", "").split("/")[0];
-}
-function getPath(url) {
-    var pth = url.replace("://", "");
-    return pth.substr(pth.indexOf("/"));
+	return url.replace("http://", "").replace("https://", "").split("/")[0];
 }
 
+function getPath(url) {
+	var pth = url.replace("://", "");
+	return pth.substr(pth.indexOf("/"));
+}
+
+// Default element to replace with loaded content
 var defaultElement = null;
+// The event to be fired when a page starts being loaded
+// Params: event (object). Fields: url
+var onPageLoading = function (event) {};
+// The event to be fired when a page is loaded
+// Params: event (object). Fields: url, status, content
+var onPageLoaded = function (event) {};
+// The event to be fired when a page encounters an error while loading
+// Params: event (object). Fields: url, status, content
+var onPageLoadError = function (event) {};
+
+// Internal vars
 var routes = {};
 var domain = getDomain(location.toString());
 var path = getPath(location.toString());
-
 var checker = null;
 
-// Loads a remote resource
-// Usage
-// String url - the url to load
-// Function ok(content) - the function that is called when the request is successful
-// (Optional) Function error(event) - the function that is called if an error occurs
-// (Optional) String method - the request method, defaults to "GET"
 function load(url, ok, error, method) {
-    var len = 0;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if(this.status == 200) {
-            if(this.responseText.length != len) {
-                ok(this.responseText);
-                len = this.responseText.length;
-            }
-        } else if(this.status > 0) {
-            if(error != undefined) {
-                error(this);
-            }
-        }
-    };
-    
-    var mthd = "GET";
-    if(method != undefined) {
-        mthd = method;
-    }
-    xhttp.open("GET", url, true);
-    xhttp.send();
+	var len = 0;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.status == 200) {
+			if (this.responseText.length != len) {
+				let obj = {
+					content: this.responseText,
+					url: this.responseURL,
+					status: this.status
+				};
+				ok(obj);
+				len = this.responseText.length;
+			}
+		} else if (this.status > 0) {
+			if (error != undefined) {
+				error(this);
+			}
+		}
+	};
+	var mthd = "GET";
+	if (method != undefined) {
+		mthd = method;
+	}
+	xhttp.open(mthd, url, true);
+	xhttp.send();
 }
 
-
-// Link hook event handler
 function linkHook(e) {
-    if(getDomain(e.target.href)==domain) {
-        e.preventDefault();
-        var loc = e.target.href.replace("://", "");
-        loc = loc.substr(loc.indexOf("/"));
-        
-        loadPage(loc);
-    }
+	if (getDomain(e.currentTarget.href) == domain) {
+		e.preventDefault();
+		var loc = e.currentTarget.href.replace("://", "");
+		loc = loc.substr(loc.indexOf("/"));
+		loadPage(loc);
+	}
 }
 
-// Applies link hooks
 function addLinkHooks() {
-    var links = document.getElementsByTagName("a");
-    for(var i = 0; i < links.length; i++) {
-        links[i].onclick = linkHook;
-    }
+	var links = document.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick = linkHook;
+	}
 }
-
-
-
-// Apply link hooks
 addLinkHooks();
 
-// Loads the specified page
 function loadPage(pth, noState, element) {
-	var elem = element;
-	if(!element) {
-		elem = defaultElement;
+	let proceed = true;
+	if (typeof (onPageLoading) == "function") {
+		var result = onPageLoading({
+			url: pth
+		});
+		if (typeof (result) == "boolean") {
+			proceed = result;
+		}
 	}
-	
-    path = pth;
-    if(!noState) window.history.pushState("{urlPath:\""+path.substr(1)+"\"}", "", path.substr(1));
-    
-    if(routes[pth]) {
-        load("/pages/"+routes[pth], function(c) {
-			if(!defaultElement) defaultElement=document.body;
-            defaultElement.innerHTML = c;
-            addLinkHooks();
-            if(checker==null) {
-                checker = setInterval(function() {
-                    var curPth = getPath(location.toString());
-                    if(curPth != path) {
-                        loadPage(curPth, true);
-                    }
-                }, 100);
-            }
-        }, function(e) console.log(e));
-    } else {
-        load("/pages/"+routes["404"], function(c) {
-			if(!defaultElement) defaultElement=document.body;
-            defaultElement.innerHTML = c;
-            addLinkHooks();
-            if(checker==null) {
-                checker = setInterval(function() {
-                    var curPth = getPath(location.toString());
-                    if(curPth != path) {
-                        loadPage(curPth, true);
-                    }
-                }, 100);
-            }
-        }, function(e) console.log(e));
-    }
+	if (proceed) {
+		var elem = element;
+		if (!element) {
+			elem = defaultElement;
+		}
+		path = pth;
+		if (!noState) window.history.pushState("{urlPath:\"" + path.substr(1) + "\"}", "", path.substr(1));
+		if (routes[pth]) {
+			load("/pages/" + routes[pth], function (c) {
+				if (!defaultElement) defaultElement = document.body;
+				defaultElement.innerHTML = c.content;
+				onPageLoaded(c);
+				addLinkHooks();
+				if (checker == null) {
+					checker = setInterval(function () {
+						var curPth = getPath(location.toString());
+						if (curPth != path) {
+							loadPage(curPth, true);
+						}
+					}, 100);
+				}
+			}, function (e) {
+				var obj = {};
+				obj.status = e.status;
+				let curUrl = e.responseURL;
+				if (curUrl.indexOf("/pages/") > 0) {
+					let secure = curUrl.startsWith("https:");
+					let path = curUrl.substr(curUrl.indexOf("/pages/") + 7);
+					let dom = getDomain(curUrl)
+					if (secure) {
+						curUrl = "https://";
+					} else {
+						curUrl = "http://";
+					}
+					curUrl += dom + "/" + path;
+				}
+				obj.url = curUrl;
+				obj.content = e.responseText;
+				onPageLoadError(obj);
+			});
+		} else {
+			load("/pages/" + routes["404"], function (c) {
+				if (!defaultElement) defaultElement = document.body;
+				defaultElement.innerHTML = c.content;
+				onPageLoaded(c);
+				addLinkHooks();
+				if (checker == null) {
+					checker = setInterval(function () {
+						var curPth = getPath(location.toString());
+						if (curPth != path) {
+							loadPage(curPth, true);
+						}
+					}, 100);
+				}
+			}, function (e) {
+				var obj = {};
+				obj.status = e.status;
+				let curUrl = e.responseURL;
+				if (curUrl.indexOf("/pages/") > 0) {
+					let secure = curUrl.startsWith("https:");
+					let path = curUrl.substr(curUrl.indexOf("/pages/") + 7);
+					let dom = getDomain(curUrl)
+					if (secure) {
+						curUrl = "https://";
+					} else {
+						curUrl = "http://";
+					}
+					curUrl += dom + "/" + path;
+				}
+				obj.url = curUrl;
+				obj.content = e.responseText;
+				onPageLoadError(obj);
+			});
+		}
+	}
 }
-
-// Load routes
-load("/routes.ini", function(c) {
-    var lines = [];
-    if(c.includes("\n")) {
-        lines = c.split("\n");
-    } else {
-        lines.push(c);
-    }
-    
-    for(var i = 0; i < lines.length; i++) {
-        if(lines[i].includes(">")) {
-            routes[lines[i].split(">")[0].trim()] = lines[i].split(">")[1].trim();
-        }
-    }
-    
-    // Load page for current path
-    loadPage(path);
-}, function(e) {
-    document.write("Failed to load routes, got status code "+e.status);
+load("/routes.ini", function (c) {
+	var lines = [];
+	if (c.content.includes("\n")) {
+		lines = c.content.split("\n");
+	} else {
+		lines.push(c.content);
+	}
+	for (var i = 0; i < lines.length; i++) {
+		if (lines[i].includes(">")) {
+			routes[lines[i].split(">")[0].trim()] = lines[i].split(">")[1].trim();
+		}
+	}
+	loadPage(path);
+}, function (e) {
+	document.write("Failed to load routes, got status code " + e.status);
 });
-
-document.onreadystatechange = function() {
-    // Apply hooks after DOM is changed
-    var observer = new MutationObserver(function(mutations) {
-    	mutations.forEach(function(mutation) {
-    		addLinkHooks();
-	    });
-    });
-    var observerConfig = {
-    	attributes: true,
-    	childList: true,
-        characterData: true 
-    };
-    var targetNode = document.body;
-    observer.observe(targetNode, observerConfig);
+document.onreadystatechange = function () {
+	var observer = new MutationObserver(function (mutations) {
+		mutations.forEach(function (mutation) {
+			addLinkHooks();
+		});
+	});
+	var observerConfig = {
+		attributes: true,
+		childList: true,
+		characterData: true
+	};
+	var targetNode = document.body;
+	observer.observe(targetNode, observerConfig);
+	addLinkHooks();
 };
